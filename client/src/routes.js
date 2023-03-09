83920b8ad959145b0df4b43ff159db6d3aa7f8b1
Sakib Siddiqi Supto <sakibsiddiqi15@gmail.com>
2023-03-09 22:54:41 +0600

    initiated the project.

---
+++ client/src/routes.js
@@ -0,0 +1,10 @@
+import { createHashRouter } from "react-router-dom";
+import Home from "./pages/Home";
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
+
+export default router;
